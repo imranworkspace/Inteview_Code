5d78142871291e175a89ca11e116162674fabd1c
imranworkspace <imranlatur24studymaterial@gmail.com>
2024-01-08 15:37:18 +0530

    cbv middleware

--- django_test/django_test/settings.py
+++ django_test/django_test/settings.py
@@ -54,7 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'middleware.fbv_middlewares.mymiddleware', # fbv middleware
+    # 'middleware.fbv_middlewares.mymiddleware', # fbv middleware
+    'middleware.cbv_middleware.Mymiddeware'  # cbv middleware
 ]
 
 ROOT_URLCONF = 'django_test.urls'
