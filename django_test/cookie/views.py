40ced2957edb4b3d83a2dab80ec5e6a54e170961
imranworkspace <imranlatur24studymaterial@gmail.com>
2024-01-08 12:48:06 +0530

    cookies used with expires attributes

--- django_test/cookie/views.py
+++ django_test/cookie/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from datetime import datetime,timedelta
 # Create your views here.
 def set_cookie(request):
     response = render(request,'stud/set.html')
-    response.set_cookie('name','imran',max_age=60)
+    # response.set_cookie('name','imran',expires=datetime.utcnow()+timedelta(milliseconds=30))
+    response.set_cookie('name','imran',expires=datetime.utcnow()+timedelta(minutes=1))
+    # response.set_cookie('name','imran',expires=datetime.utcnow()+timedelta(days=2))
     return response
     
 def get_cookie(request):
