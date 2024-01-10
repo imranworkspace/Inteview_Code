d057e007a16ab4d102e0ff1b508a143d1fe8265c
imranworkspace <imranlatur24studymaterial@gmail.com>
2024-01-10 21:31:41 +0530

    DTL date update

--- django_test/templates/views.py
+++ django_test/templates/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from datetime import datetime
 # Create your views here.
 def home(request):
     context={
-        'name':'imran'
+        'name':'imran',
+        'dt':datetime.now()
     }
     return render(request,'index.html',context)
     # return render(request,'index.html',context,content_type='application/xhtml/xml') # if you want to download this file use content_type
