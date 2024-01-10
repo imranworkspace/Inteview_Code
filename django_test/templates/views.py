79633ca903627b1b93710de336b07941e9ddd98a
imranworkspace <imranlatur24studymaterial@gmail.com>
2024-01-10 20:38:08 +0530

    small update in views

--- django_test/templates/views.py
+++ django_test/templates/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'index.html')
+    context={
+        'name':'imran'
+    }
+    return render(request,'index.html',context)
+    # return render(request,'index.html',context,content_type='application/xhtml/xml') # if you want to download this file use content_type
