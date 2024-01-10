c6c70de7c095d720d44a6a5d3b7d67da81d80ff8
imranworkspace <imranlatur24studymaterial@gmail.com>
2024-01-10 21:33:59 +0530

    update branch and DTL with date

--- django_test/templates/views.py
+++ django_test/templates/views.py
@@ -5,6 +5,7 @@ def home(request):
     context={
         'name':'imran',
         'dt':datetime.now()
+        
     }
     return render(request,'index.html',context)
     # return render(request,'index.html',context,content_type='application/xhtml/xml') # if you want to download this file use content_type
